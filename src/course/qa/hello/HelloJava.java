b35481db3b10c6d38d773dd08f7008229adcda62
Tanya Yovkova <tanita.yovkova@gmail.com>
2022-04-20 15:31:51 +0300

    Created java project

---
+++ src/course/qa/hello/HelloJava.java
@@ -0,0 +1,12 @@
+package course.qa.hello;
+
+import java.util.Scanner;
+
+public class HelloJava {
+    public static void main(String[] args) {//nqma nujda ot obekt za da se startira
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();//prochita imeto ot konzolata
+        System.out.println("Hello " + name + " from java 11");
+    }
+}
