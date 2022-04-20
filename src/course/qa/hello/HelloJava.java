c084dbdb1e03e61e9348d459262bd18fa153bb8e
Tanya Yovkova <tanita.yovkova@gmail.com>
2022-04-20 16:26:19 +0300

    error

--- src/course/qa/hello/HelloJava.java
+++ src/course/qa/hello/HelloJava.java
@@ -8,5 +8,6 @@ public class HelloJava {
         System.out.println("Enter your name:");
         String name = sc.nextLine();//prochita imeto ot konzolata
         System.out.println("Hello " + name + " from java 11");
+       
     }
 }
