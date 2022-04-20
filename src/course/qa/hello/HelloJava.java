7283b7216183f2fcf903e63beb0d30e51c9e16dd
Tanya Yovkova <tanita.yovkova@gmail.com>
2022-04-20 18:04:18 +0300

    ex formatter

---
+++ src/course/qa/hello/HelloJava.java
@@ -0,0 +1,15 @@
+package course.qa.hello;
+import course.qa.feature.HelloFormatter;
+
+import java.util.Scanner;
+
+public class HelloJava {
+    public static void main(String[] args) {//nqma nujda ot obekt za da se startira
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();//prochita imeto ot konzolata
+        HelloFormatter hf = new HelloFormatter();
+        System.out.println(hf.format(name));
+
+    }
+}
